6fc9f066ff479d2bf2118dd3d3aef4f09f2c4f98
Andrey Rakhmatullin <wrar@wrar.name>
2023-03-15 22:43:55 +0400

    Cast Curiosity.

--- src/tasks/leveling.ts
+++ src/tasks/leveling.ts
@@ -116,6 +116,9 @@ const usefulEffects: Effect[] = [
 
   // Spell dmg
   $effect`Carol of the Hells`,
+
+  // Famwt
+  $effect`Curiosity of Br'er Tarrypin`,
 ];
 
 export function powerlevelingLocation(): Location {
