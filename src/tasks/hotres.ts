87620c3755b2a9a26791b1d84ad96033952202ca
Andrey Rakhmatullin <wrar@wrar.name>
2023-04-08 17:59:44 +0400

    Save silver face paint.

--- src/tasks/hotres.ts
+++ src/tasks/hotres.ts
@@ -164,7 +164,7 @@ export const HotResQuest: Quest = {
           $effect`Blood Bond`,
           $effect`Empathy`,
           $effect`Leash of Linguini`,
-          $effect`Robot Friends`,
+          // $effect`Robot Friends`,
         ];
         usefulEffects.forEach((ef) => tryAcquiringEffect(ef, true));
         handleCustomPulls("instant_hotTestPulls", hotTestMaximizerString);
